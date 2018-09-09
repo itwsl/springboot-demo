fc9f28f5ed2c1e0c500f14445b82c0b637bc3142
songlin <itwsl@sina.cn>
2018-09-09 22:08:17 +0800

    init

---
+++ src/main/java/com/springboot/demo/HelloController.java
@@ -0,0 +1,17 @@
+package com.springboot.demo;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by wsl on 2018/9/9.
+ */
+@RestController
+@EnableAutoConfiguration
+public class HelloController {
+    @RequestMapping("/hello")
+    private String index(){
+        return "Hello World!";
+    }
+}
